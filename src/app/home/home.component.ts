f62745c1f177b994be7c2e82a5eadbee09048133
seanmksha <seanmakosharma@gmail.com>
2019-02-03 17:09:57 -0800

    Hooking up AuthService with home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from './../auth/auth.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  user: any = {};
+  private userSubscription: Subscription;
+  
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+    this.authService.me().subscribe(data=>{
+      this.user=data.user;
+    });
+    // update this.user after login/register/logout
+    this.userSubscription = this.authService.$userSource.subscribe((user)=>{
+      this.user=user;
+    });
+    
   }
 
 }
